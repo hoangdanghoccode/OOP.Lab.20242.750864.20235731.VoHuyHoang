d773d4a36e5d3be4cd0191d75d4fcbdcfe60f02d
Hoang Vo <hhtdv2407@gmail.com>
2025-03-30 23:45:03 +0700

    finish section 8

---
+++ lab02/Design/code/AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,10 @@
+
+public class DigitalVideoDisc {
+      public String title;
+      public String category;
+      public String director;
+      public int length;
+      public float cost;
+      
+      
+}
