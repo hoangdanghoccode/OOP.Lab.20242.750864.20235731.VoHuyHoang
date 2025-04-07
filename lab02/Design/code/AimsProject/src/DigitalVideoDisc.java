72e3fc8e15b6104870409f836e05cf60225d1a66
Hoang Vo <hhtdv2407@gmail.com>
2025-04-07 11:35:04 +0700

    finish section 16

--- lab02/Design/code/AimsProject/src/DigitalVideoDisc.java
+++ lab02/Design/code/AimsProject/src/DigitalVideoDisc.java
@@ -5,6 +5,10 @@ public class DigitalVideoDisc {
       private String director;
       private int length;
       private float cost;
+      private int id;
+      private static int nbDigitalVideoDiscs = 0;
+      
+      
 	public String getTitle() {
 		return title;
 	}
@@ -23,12 +27,16 @@ public class DigitalVideoDisc {
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
+		nbDigitalVideoDiscs++;      
+		this.id = nbDigitalVideoDiscs; 
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;      
+		this.id = nbDigitalVideoDiscs; 
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
@@ -36,6 +44,8 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;      
+		this.id = nbDigitalVideoDiscs; 
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
@@ -44,10 +54,13 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;      
+		this.id = nbDigitalVideoDiscs; 
 	}
 	public void setTitle(String title) {
 		this.title = title;
 	}
-      
+	
+	
       
 }
