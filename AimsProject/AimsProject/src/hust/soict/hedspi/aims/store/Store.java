eba069481436ae2e9bfa74faaf5d0d399eedd721
Hoang Vo <hhtdv2407@gmail.com>
2025-04-27 00:48:44 +0700

    finish bai 5

---
+++ AimsProject/AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -0,0 +1,45 @@
+package hust.soict.hedspi.aims.store;
+
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+
+public class Store {
+  private static final int MAX_ITEMS = 100;
+  private DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[MAX_ITEMS];
+  private int qty = 0;
+  
+  public void addDVD(DigitalVideoDisc disc) {
+	  if (qty >= MAX_ITEMS) {
+          System.out.println("The store is full. Cannot add more DVDs.");
+      } else {
+          itemsInStore[qty] = disc;
+          qty++;
+          System.out.println(disc.getTitle() + " has been added to the store.");
+      }
+}
+  public void removeDVD(DigitalVideoDisc disc) {
+      boolean found = false;
+      for (int i = 0; i < qty; i++) {
+          if (itemsInStore[i].equals(disc)) {
+              found = true;
+              for (int j = i; j < qty - 1; j++) {
+                  itemsInStore[j] = itemsInStore[j + 1];
+              }
+              itemsInStore[qty - 1] = null;
+              qty--;
+              System.out.println(disc.getTitle() + " has been removed from the store.");
+              break;
+          }
+      }
+      if (!found) {
+          System.out.println("DVD not found in the store.");
+      }
+  }
+  
+  public void displayStore() {
+      System.out.println("DVDs available in the store:");
+      for (int i = 0; i < qty; i++) {
+          System.out.println((i + 1) + ". " + itemsInStore[i].toString());
+      }
+  }
+  
+}
---
+++ AimsProject/AimsProject/src/hust/soict/hedspi/test/store/StoreTest.java
@@ -0,0 +1,29 @@
+package hust.soict.hedspi.test.store;
+
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+import hust.soict.hedspi.aims.store.Store;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        Store myStore = new Store();
+
+        
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 124, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "John Musker", 90, 18.99f);
+
+        
+        myStore.addDVD(dvd1);
+        myStore.addDVD(dvd2);
+        myStore.addDVD(dvd3);
+
+        
+        myStore.displayStore();
+
+       
+        myStore.removeDVD(dvd2);
+
+        
+        myStore.displayStore();
+    }
+}
